22ce5fb299cd3ec53365f4f313b3dcf7abf34fb7
Michael Kibet <mikebet87@gmail.com>
2023-02-19 09:47:45 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line using print f
+ *
+ * Descripton - a program that prints a line using print f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
