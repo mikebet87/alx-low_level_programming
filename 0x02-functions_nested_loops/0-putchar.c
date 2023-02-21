63e59dc40b6c5d9c149090e08ab718a0b5d7e694
Michael Kibet <mikebet87@gmail.com>
2023-02-21 09:01:38 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,9 @@
  */
 
 int main(void)
-{
-	_putchar("_");
+{	
+	
+        _putchar("_");
 	_putchar("p");
 	_putchar("u");
 	_putchar("t");
@@ -18,4 +19,5 @@ int main(void)
 	_putchar("\n");
 
 	return (0);
+
 }
