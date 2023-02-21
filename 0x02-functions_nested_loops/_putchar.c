550bfe9daa010766c6a1de9e336ac5fa2affb27d
Michael Kibet <mikebet87@gmail.com>
2023-02-21 08:42:51 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to standout
+ * @c : the character to print
+ * Return: on success 1
+ *
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
